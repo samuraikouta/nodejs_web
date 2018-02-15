6d201e1aefd97a76aa6e46ae28817d186e19b2ea
vagrant <vagrant@localhost.localdomain>
2018-02-15 12:42:53 +0000

    getpage.jsの追加

---
+++ 02/getpage.js
@@ -0,0 +1,16 @@
+// モジュールの取り込み
+var client = require('cheerio-httpcli');
+
+// ダウンロード
+var url = "http://www.aozora.gr.jp/index_pages/person81.html";
+var param = {};
+client.fetch(url, param, function(err, $, res){
+    // エラーが無いかチェック
+    if(err){ 
+        console.log("Error:", err);
+        return; 
+    }
+    // ダウンロードした結果を画面に表示
+    var body = $.html();
+    console.log(body);
+});
